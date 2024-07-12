4b3ef26597b5e39e6445f1fd7525a6306d87bf93
RoaringInferno <jj2t17@gmail.com>
2024-07-12 10:35:23 -0400

    Segfaulting

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include "input_parsing.hpp"
 #include "daemon.hpp"
+
 #include <iostream>
 
-int main(int argc, char** argv) {
+void eval_operation(HashFunctionTree& tree, const uint8_t operation_id)
+{
+    
+}
+
+int main(int argc, char** argv)
+{
     Args args;
     parse_args(args, argc, argv);
     std::cout << "For closed set of strings:\n";
@@ -13,14 +20,16 @@ int main(int argc, char** argv) {
 
     HashFunctionTree tree(args.args, args.count, args.depth);
 
-    for (operation_index_t i = 1; i <= args.depth; i++) {
-        for (uint8_t operation_id = 0; operation_id < OPERATION_COUNT; operation_id++)
+    for (uint8_t operation_id = 0; operation_id < OPERATION_COUNT; operation_id++) {
+        //eval_operation(tree, operation_id);
+        for (operation_index_t i = 1; i <= tree.get_total_depth(); i++)
         {
             Operation operation = OPERATIONS[operation_id]; // Get the operation
             operation_index_t depth_to_search = i-1; // The depth to search is the previous layer
             
             tree.evaluate(operation, depth_to_search); // Evaluate the operation
         }
+        std::cout << "Tree created\n"; // Debugging
     }
 
     Symbol fastest = tree.get_fastest();
